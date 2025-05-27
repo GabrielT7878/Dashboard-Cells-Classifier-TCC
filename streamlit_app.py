e43ab457da007716dab74f2f6d41f4dd8d22ae60
Gabriel Thiago <97263124+GabrielT7878@users.noreply.github.com>
2025-05-27 16:01:07 -0300

    modify upload page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import cv2
 import uuid
+import os
 
 st.set_page_config(initial_sidebar_state="collapsed")
 
 st.header("Classificador de Células em Mitose")
-st.subheader("Faça o upload de imagens de células ou use as imagens de exemplo clicando no botão abaixo")
+st.subheader("Faça o upload de imagens (Formato JPG) de células ou use as imagens de exemplo clicando no botão abaixo")
 
 if st.button("Usar Imagens exemplo"):
     st.session_state['example_images'] = True
@@ -28,8 +29,9 @@ st.markdown("---")
 if st.button("Continuar"):
 
     if files:
+        images_path = f"./uploaded_images/{str(uuid.uuid4())}"
+        os.makedirs(images_path, exist_ok=True)
         for i, image in enumerate(files):
-            images_path = f"./uploaded_images/{str(uuid.uuid4())}"
             with open(f"{images_path}/{image.name}", "wb") as f:
                 f.write(image.read())
         st.session_state['example_images'] = False
